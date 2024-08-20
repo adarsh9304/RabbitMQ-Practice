69890410861f6882f23f4dcb7637ec687be7dce7
Bacancy <adarsh.patel@bacancy.com>
2024-08-20 15:56:07 +0530

    Adds:basic of RabbitMQ

---
+++ consumer.js
@@ -0,0 +1,23 @@
+const amqp = require('amqplib/callback_api');
+
+const queue = 'hello';
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+
+    channel.assertQueue(queue, { durable: false });
+
+    console.log(" messages will be displayed here: ", queue);
+
+    channel.consume(queue, (msg) => {
+      console.log(" Message is : ", msg.content.toString());
+    }, { noAck: true });
+  });
+});
